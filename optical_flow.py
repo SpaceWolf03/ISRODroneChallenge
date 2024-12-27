fb4c45226e69e9f1ab282cdd96b00ec53d1a16f6
Hrushikesh Patwardhan <hrushikeshp13@gmail.com>
2024-12-27 13:23:12 +0530

    update optical flow code

--- optical_flow.py
+++ optical_flow.py
@@ -13,17 +13,22 @@ feature_params = dict( maxCorners = 100,
                        minDistance = 7,
                        blockSize = 7 )
 # Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (15, 15),
-                  maxLevel = 2,
+lk_params = dict( winSize  = (20, 20),
+                  maxLevel = 3,
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 # Create some random colors
 color = np.random.randint(0, 255, (100, 3))
 # Take first frame and find corners in it
-ret, old_frame = cap.read()
-ret, old_frame = cap.read()
-ret, old_frame = cap.read()
+
+for i in range(100):
+    ret, old_frame = cap.read()
+
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+
+if p0.size > 2:               # limit the number of tracking points to 2
+    p0 = p0[0:2]
+
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
 while(1):
@@ -52,4 +57,6 @@ while(1):
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1, 1, 2)
-    cv2.destroyAllWindows()
+
+cap.release()
+cv2.destroyAllWindows()
